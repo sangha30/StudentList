a5e5291730d0693accbbdc65af064d69b213cf03
sangh <sangh@10.48.42.153>
2024-09-16 09:30:45 -0400

    Student.java updated

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -12,6 +12,7 @@ public class Student {
     private String name;
     private String studentID;
     private String address;
+    private String employeed;
 
     public String getName() {
         return name;
