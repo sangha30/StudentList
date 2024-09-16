99880ca4af1c06fa013cc05c02eddad6210ce9ad
sangh <sangh@10.48.42.153>
2024-09-16 10:38:19 -0400

    PartTimeStudent file added

---
+++ src/studentlist/PartTimeStudent.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author sangh
+ */
+public class PartTimeStudent extends Student{
+    private int NumCourses;
+
+    public int getNumCourses() {
+        return NumCourses;
+    }
+
+    public void setNumCourses(int NumCourses) {
+        this.NumCourses = NumCourses;
+    }
+}
